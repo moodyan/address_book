d965e794f3988521f6f071e45d82dbaf24337817
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-26 18:28:41 -0400

    Add delete contact method.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,10 +10,10 @@ namespace AddressBook
     public HomeModule()
     {
       Get["/"] = _ => {
-      List<Contact> allContacts = Contact.GetAll();
-      return View["index.cshtml", allContacts];
+        List<Contact> allContacts = Contact.GetAll();
+        return View["index.cshtml", allContacts];
       };
-      Get["/contact/new"] = _ => {
+        Get["/contact/new"] = _ => {
         return View["contact_form.cshtml"];
       };
       Post["/contact"] = _ => {
@@ -25,10 +25,15 @@ namespace AddressBook
         return View["/contact.cshtml", contact];
       };
       Post["/contact/cleared"] = _ => {
-       Contact.Clear();
-       return View["contacts_cleared.cshtml"];
-     };
-
+        Contact.Clear();
+        return View["contacts_cleared.cshtml"];
+      };
+      Post["/contact/{id}"] = parameters => {
+        Contact contact = Contact.Find(parameters.id);
+        contact.Remove();
+        List<Contact> allContacts = Contact.GetAll();
+        return View["contact_removed.cshtml", allContacts];
+      };
     }
   }
 }
