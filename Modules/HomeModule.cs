3823802b4556a19eb85d0f399f613970d3723f0a
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-26 17:12:04 -0400

    Redo Contacts.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,9 +9,22 @@ namespace AddressBook
     public HomeModule()
     {
       Get["/"] = _ => {
-      // List<Contacts> allContacts = Contacts.GetAll();
-      return View["index.cshtml"];
+      List<Contacts> allContacts = Contacts.GetAll();
+      return View["index.cshtml", allContacts];
       };
+      Get["/contacts"] = _ => {
+        List<Contacts> allContacts = Contacts.GetAll();
+        return View["contacts.cshtml", allContacts];
+      };
+      Get["/contacts/new"] = _ => {
+        return View["contact_form.cshtml"];
+      };
+      Post["/contacts/added"] = _ => {
+        Contacts newContact = new Contacts(Request.Query["name"]);
+        List<Contacts> allContacts = Contacts.GetAll();
+        return View["contact_added.cshtml", allContacts];
+      };
+
 
     }
   }
--- Objects/Contacts.cs
+++ Objects/Contacts.cs
@@ -2,31 +2,58 @@ using System.Collections.Generic;
 
 namespace AddressBook.Objects
 {
-  public class Contacts
+  public class Contact
   {
-    private static List<Contacts> _contacts = new List<Contacts>{};
-    private string _contact;
+    private static List<Contact> _contacts = new List<Contact>{};
+    private string _name;
+    private string _phone;
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private int _zip;
     private int _id;
-    private List<People> _listPeople;
-    public Contacts(string contactType)
+=
+    public Contact(string name, string phone, string streetAddress, string city, string state, string zip)
     {
-      _contact = contactType;
+      _name = name;
+      _phone = phone;
+      _streetAddress = streetAddress;
+      _city = city;
+      _state = state;
+      _zip = zip;
       _contacts.Add(this);
       _id = _contacts.Count;
-      _listPeople = new List<People>{};
     }
 
-    public string GetContact()
+    public string GetName()
     {
-      return _contact;
+      return _name;
     }
-
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public string GetStreetAddress()
+    {
+      return _streetAddress;
+    }
+    public string GetCity()
+    {
+      return _city;
+    }
+    public string GetState()
+    {
+      return _state;
+    }
+    public int GetZip()
+    {
+      return _zip;
+    }
     public int GetId()
     {
       return _id;
     }
-
-    public static List<Contacts> GetAll()
+    public static List<Contact> GetAll()
     {
       return _contacts;
     }
@@ -35,20 +62,14 @@ namespace AddressBook.Objects
     {
       _contacts.Clear();
     }
+    public void Remove()
+    {
+      _contacts.Remove(this);
 
-    public static Contacts Find(int searchID)
+
+    public static Contact Find(int searchID)
     {
       return _contacts[searchID - 1];
-    }
-    //view all People in a collection
-    public List<People> GetPeople()
-    {
-      return _listPeople;
-    }
-    //Add a new CD to a collection
-    public void AddPeople(People people)
-    {
-      _listPeople.Add(people);
     }
   }
 }
